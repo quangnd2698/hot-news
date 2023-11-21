bf687a62af8464798113b9d542570727eb2852e4
quangnd <quangnd261998@gmail.com>
2023-11-21 22:58:02 +0700

    fix short icon

--- resources/views/site/inc/head.blade.php
+++ resources/views/site/inc/head.blade.php
@@ -8,13 +8,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="index, follow">
     <meta name="title" content="{{ !empty($metaTitle) ? $metaTitle . '- Tiêu điểm 365' : 'Tiêu điểm 365 - Tin tức mới nhất trong ngày' }}">
+   
+    <meta property="og:description" content="{{ !empty($metaDescription) ? $metaDescription : 'Tin nhanh nơi tổng hợp các tin tức mới nhất, chính xác nhất trong ngày. Đừng bỏ lỡ bất kỳ thông tin nào nhé' }}">
+    <meta name="keywords" content="{{ !empty($metaTitle) ? $metaTitle : ''}} Tin hot trong ngày, tin tức trong ngày, tin hot trong ngay, tin moi trong ngay, Tiêu điểm 365, tieudiem365, tieudiem365.site, tieu diem 365">
+    <link rel="shortcut icon" type="image/x-icon" href="/site/assets/img/favicon.png">
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{env('APP_URL')}}">
     <meta property="og:image" content="{{(!empty($news) && !empty($news->image_url)) ? $news->image_url : '/site/assets/img/favicon.png'}}" />
     <meta property="og:title" content="{{ !empty($metaTitle) ? $metaTitle . '- Tiêu điểm 365' : 'Tiêu điểm 365 - Tin tức mới nhất trong ngày' }}"/>
-    <meta property="og:description" content="{{ !empty($metaDescription) ? $metaDescription : 'Tin nhanh nơi tổng hợp các tin tức mới nhất, chính xác nhất trong ngày. Đừng bỏ lỡ bất kỳ thông tin nào nhé' }}">
-    <meta name="keywords" content="{{ !empty($metaTitle) ? $metaTitle : ''}} Tin hot trong ngày, tin tức trong ngày, tin hot trong ngay, tin moi trong ngay, Tiêu điểm 365, tieudiem365, tieudiem365.site, tieu diem 365">
-    <link rel="shortcut icon" type="image/x-icon" href="/site/assets/img/favicon.png">
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2479266761406973"
         crossorigin="anonymous"></script>
     <!-- Google tag (gtag.js) -->
